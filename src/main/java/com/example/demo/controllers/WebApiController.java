20f1a41f2c8b1a2c8ec1d73dd74e94e16f38cf26
sato-t <sato.takayuki@brainnet.co.jp>
2023-10-13 13:49:50 +0900

    コントローラのパッケージ名を変更

--- src/main/java/com/example/demo/controllers/WebApiController.java
+++ src/main/java/com/example/demo/controllers/WebApiController.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller;
+package com.example.demo.controllers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PathVariable;
